58ec0e29a11f7cd9cf36d571647486e077fd9662
nkumar125 <38634288+nkumar125@users.noreply.github.com>
2021-06-14 10:05:23 -0400

    oath working

--- src/server/controllers/apicontroller.js
+++ src/server/controllers/apicontroller.js
@@ -4,16 +4,46 @@ const { resolve } = require('path');
 const path = require('path');
 const { useParams } = require('react-router');
 
+const fetch = require("node-fetch")
+
 const db = require('../models/dbmodels');
 
 const apiController = {};
 
 // OAuth log in
-apiController.login = (req, res, next) => {
-  console.log(req.params);
+apiController.login = async (req, res, next) => {
+//   console.log("params", req.params.token);
+  
+  try {
+    const token = req.params.token;
+    console.log("token",token)
+
+    const tokenVar = `token ${token}`
+    console.log("hi", tokenVar)
+    // const options = {
+    //   method: "GET",
+    //   headers: {"Authorization": `token${token}`}
+    // }
+  
+    let data = await fetch(`https://api.github.com/user`, {method: 'GET', headers: {Authorization: tokenVar}});
+
+    data = await data.json()
+
+
+    console.log(data)
+
+    let username = data
+
+    // post to users table in db
+    return next()
+
+  } catch(err) {
+    console.log(err);
+    return next(err);
+  }
   // query for adding user data
-  // const sql = 'INSERT INTO users VALUES(DEFAULT, $1)';
-  // parameterize query - query(sql, array containing token)
+  // const sql = 'INSERT INTO users VALUES(DEFAULT, $1, $2)';
+  // parameterize query - query(sql, array [token, username])
   // on sucessful login - redirect /dashboard
 };
 
